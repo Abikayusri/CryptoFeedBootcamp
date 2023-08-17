29fc24daaad641eebb47b42fa7463a1b35744535
Abika Chairul Yusri <acyusri@hitachi-ch.co.id>
2023-08-17 12:46:46 +0700

    first commit

---
+++ app/src/main/java/abika/sinau/mycryptofeed/MainActivity.kt
@@ -0,0 +1,46 @@
+package abika.sinau.mycryptofeed
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import abika.sinau.mycryptofeed.ui.theme.MyCryptoFeedTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MyCryptoFeedTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    MyCryptoFeedTheme {
+        Greeting("Android")
+    }
+}
